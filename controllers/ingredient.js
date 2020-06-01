6f2893005eaff627e83e5787adda111d48701600
connorj1002 <connorj1002@gmail.com>
2020-06-01 11:19:13 -0600

    adds delete route to ingredient controllers

--- controllers/ingredient.js
+++ controllers/ingredient.js
@@ -33,54 +33,16 @@ router.post("/", function (req, res) {
   });
 });
 
-/* Ingredient show route */
-router.get("/:id",  function (req, res) {
-  db.Ingredient.findById(req.params.id, function (error, foundIngredient) {
+/* Delete ingredient */
+router.delete("/:id",  function (req, res) {
+  db.Ingredient.findByIdAndDelete(req.params.id, function (error, deletedIngredient) {
     if(error) {
       console.log(error);
       res.send({message: "Internal Server Error"});
     } else {
-      const context = {ingredients: foundIngredient};
-      res.render("ingredients/show", context);
+      res.redirect("/ingredients");
     }
   });
 });
-
-/* Edit bar route */
-// router.get("/:id/edit",  function (req, res) {
-//   db.Bar.findById(req.params.id, function (error, foundBar) {
-//     if(error) {
-//       console.log(error);
-//       res.send({message: "Internal Server Error"});
-//     } else {
-//       const context = {bars: foundBar};
-//       res.render("bars/edit", context);
-//     }
-//   });
-// });
-
-/* Update bar */
-// router.put("/:id",  function (req, res) {
-//   db.Bar.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (error, updatedBar) {
-//     if(error) {
-//       console.log(error);
-//       res.send({message: "Internal Server Error"});
-//     } else {
-//       res.redirect(`bars/${updatedBar._id}`);
-//     }
-//   });
-// });
-
-/* Delete bar */
-// router.delete("/:id",  function (req, res) {
-//   db.Bar.findByIdAndDelete(req.params.id, function (error, deletedBar) {
-//     if(error) {
-//       console.log(error);
-//       res.send({message: "Internal Server Error"});
-//     } else {
-//       res.redirect("/bars");
-//     }
-//   });
-// });
 
 module.exports = router;
