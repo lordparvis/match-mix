00248a3832e1182aed24498f9180730826cbd08c
connorj1002 <connorj1002@gmail.com>
2020-05-31 22:22:52 -0600

    adds post route for new bars

--- controllers/bar.js
+++ controllers/bar.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const db = require("../models");
 
+/* Root route /bars */
 router.get("/", async function (req, res) {
   try {
     const allBars = await db.Bar.find({});
@@ -11,6 +12,23 @@ router.get("/", async function (req, res) {
     console.log(error);
     res.send({message: "Internal Server Error"});
   }
+});
+
+/* New Bar route */
+router.get("/new", function (req, res) {
+  res.render("bars/new");
+});
+
+/* Create new bar */
+router.post("/", function (req, res) {
+  db.Bar.create(req.body, function (error, createdBar) {
+    if(error) {
+      console.log(error);
+      res.send({message: "Internal Server Error"});
+    } else {
+      res.redirect("/bars");
+    }
+  });
 });
 
 module.exports = router;
